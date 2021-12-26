272f190a04ec246b7cd28cf1f32db03b0e740a46
Zac Smith <zrs3141592@gmail.com>
2021-12-26 12:25:02 -0700

    remove

---
+++ 03/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const clearConsole = require('clear-any-console')
+clearConsole()
+// console.clear()
+console.log(`
+Mr Zac Smith
+
+Node engineer that researchs secure and efficient microserivce back-end using Node.js and Express.js.
+
+🐦 Twitter:  https://twitter.com/MrZacSmith
+💻 GitHub:   https://github.com/mrzacsmith
+
+`)
---
+++ README.md
@@ -0,0 +1,4 @@
+# Notes
+
+- use `chmod +x <filename>` to make executable and the includ `#!/usr/bin/emv node` and run file using `./<filename>`
+- best: `npm link` and `npm uninstall -g <name>`
